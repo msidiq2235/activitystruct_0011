876c6956029c66775c462766558bdafb46bc6acb
msidiq2235 <muhsidiq2005@gmail.com>
2024-04-02 17:36:54 +0700

    11

--- StrukturC_2/StrukturC_2.cpp
+++ StrukturC_2/StrukturC_2.cpp
@@ -20,4 +20,11 @@ int main() {
 	cout << "Nama Mahasiswa : ";
 	cin >> mhs.nama;
 
+	cout << "Alamat Mahasiswa : " << endl;
+	cout << "\t Nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama Kota : ";
+	cin >> mhs.alamat.kota;
+
+	
 }
