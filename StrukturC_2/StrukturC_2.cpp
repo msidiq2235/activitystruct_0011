b3b74461a4b43bfa50c28aa5e8084dc48be2d1c6
msidiq2235 <muhsidiq2005@gmail.com>
2024-04-02 16:51:07 +0700

    5

--- StrukturC_2/StrukturC_2.cpp
+++ StrukturC_2/StrukturC_2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
 struct Mahasiswa {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	int umur;
 };
